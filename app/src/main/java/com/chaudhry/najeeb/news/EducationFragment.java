8248224a98c2cd13d8d2ae8b135e7f9b22a492e1
Najeeb <najeeb2kz@yahoo.com>
2017-02-22 06:43:27 -0500

    Initial commit

---
+++ app/src/main/java/com/chaudhry/najeeb/news/EducationFragment.java
@@ -0,0 +1,27 @@
+package com.chaudhry.najeeb.news;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EducationFragment extends Fragment {
+
+
+    public EducationFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/chaudhry/najeeb/news/ImmigrationFragment.java
@@ -0,0 +1,34 @@
+package com.chaudhry.najeeb.news;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ImmigrationFragment extends Fragment {
+
+
+    public ImmigrationFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
+
+
+
+
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
